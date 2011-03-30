25c177fa774c6546d7ae791dbe90d9bc500a2af3
Helmy Giacoman <helmygb@gmail.com>
2011-03-30 16:54:43 -0600

    menus casi listos

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
 SITE_ID = 1
 
@@ -91,7 +91,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'treemenus',
+    'cieets.treemenus',            
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- urls.py
+++ urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    # Uncomment the next line to enable the admin:    
     url(r'^admin/', include(admin.site.urls)),
 )
