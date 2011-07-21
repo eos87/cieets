664f2f05efa909dcb1cdc9d1f1c8037990ea8614
Helmy Giacoman <helmygb@gmail.com>
2011-07-20 22:26:03 -0600

    fix a tildes

---
+++ contenido/templatetags/tools.py
@@ -0,0 +1,12 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def cufon(value):
+    print value.split(' ')
+    lista = []
+    for word in value.split(' '):
+        palabra = '<span class="caps">%s</span>%s' % (word[0], word[1:])        
+        print palabra
+        lista.append(palabra)       
+    return ' '.join(lista)
