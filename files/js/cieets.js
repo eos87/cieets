051ad7729121c72fbdea3ebd082281f0aa795f35
Helmy Giacoman <helmygb@gmail.com>
2011-07-20 19:51:45 -0600

    pimpiando el menu

--- files/js/cieets.js
+++ files/js/cieets.js
@@ -1,24 +1,19 @@
-function mainmenu(){
-    var elems = $('.nivel2');
-    for(var i=0;i<elems.length;i++){
-        var ancho = 0;
-        var hijos = $(elems[i]).children();
-        for(var j=0; j<hijos.length;j++){
-            ancho += $(hijos[j]).width();
-        }
-        $(elems[i]).width(ancho);
-    }
-    $('.nivel2').each(function(){
-        var ancho = 0;
-        $(this).children().each(function(){
-            ancho += $(this).width();
-        });
-        $(this).css('width', ancho+'px');
-    });
+function mainmenu(){    
     $("#nav ul").css({
         display: "none"
-    }); // Opera Fix
-    $("#nav li").hover(function(){
+    }); // Opera Fix   
+}
+$(document).ready(function(){
+	//asignar tamaños cuando carga la pagina
+	$('.nivel2').each(function(){
+    	var ancho = 0;
+    	$(this).children().each(function(){
+    		ancho = ancho + $(this).width();
+    	})
+    	$(this).width(ancho);
+    });
+	//codigo para mostrar el menu
+	$("#nav li").hover(function(){
         $(this).find('ul:first').css({
             visibility: "visible",
             display: "none"
@@ -28,18 +23,16 @@ function mainmenu(){
             visibility: "hidden"
         });
     });
-}
-$(document).ready(function(){
-    mainmenu();    
+    mainmenu();   
 });
-$(window).load(function(){
+$(window).load(function(){	
     var sidebar = $('#sidebar').height();
     var cuerpo = $('#cuerpo').height();
     if(cuerpo>=sidebar){
         $('#sidebar').height(cuerpo);
     }else{
         $('#cuerpo').height(sidebar);
-    }
+    }    
 });
 
 
